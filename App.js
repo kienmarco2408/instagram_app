398c05ae59c623d0bec8ee86c04b64e6312d6802
kienmarco2408 <kienmarco999@gmail.com>
2022-08-01 18:40:44 +0700

    update

--- App.js
+++ App.js
@@ -1,10 +1,26 @@
+import React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Ionic from 'react-native-vector-icons/Ionicons';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Lobster_400Regular, useFonts } from '@expo-google-fonts/lobster';
+import AppLoading from 'expo-app-loading';
 
 export default function App() {
+  let [fontsLoaded, error] = useFonts({
+    Lobster_400Regular,
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={{ fontFamily: 'Lobster-Regular' }}>
+        Open up App.js to start working on your app!
+      </Text>
+      <Ionic name="play" style={{ fontSize: 20 }} />
       <StatusBar style="auto" />
     </View>
   );
